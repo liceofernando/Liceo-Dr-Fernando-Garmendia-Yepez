da8dde3ec37b3be6a44c5ec88104d3cf1f25649d
Luicyber <1005.25653055.ucla@gmail.com>
2025-01-14 11:40:43 -0400

    modificacion del modal de calendario

--- src/routes/Calendario.jsx
+++ src/routes/Calendario.jsx
@@ -117,44 +117,40 @@ const Calendario = () => {
       <div className="container d-flex justify-content-center my-4" style={{minHeight: '300px'}}>
         {showDetails && selectedEvent && (
           <div className="" style={{ maxWidth: '500px' }}>
-         
-            <a
- 
-  className="relative block overflow-hidden rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8"
->
-  <span
-    class="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-red-300 via-blue-500 to-red-600"
-  ></span>
-
-  <div class="sm:flex sm:justify-between sm:gap-4">
-    <div>
-      <h3 class="text-lg font-bold text-gray-900 sm:text-xl">
-      {selectedEvent.title}
-      </h3>
-    </div>
-    <button onClick={handleClose} style={{ float: 'right', border: 'none', background: 'none', cursor: 'pointer' }}>
-                <span style={{ fontSize: '20px', color: 'red' }}>X</span>
+          <button 
+                className="bg-red-600 hover:bg-red-500 text-white font-semibold hover:text-gray-600 py-2 px-6 border border-white hover:border-transparent rounded mb-2" 
+                onClick={handleClose}> {/* Cambia aquí para usar handleClose */}
+                Cerrar
               </button>
-  </div>
-
-  <div class="mt-4">
-    <p class="text-pretty text-sm text-gray-500">
-    {selectedEvent.description}
-    </p>
-  </div>
-
-  <dl class="mt-6 flex gap-4 sm:gap-6">
-    <div class="flex flex-col-reverse">
-      <dt class="text-sm font-medium text-gray-600">Fecha de inicio</dt>
-      <dd class="text-xs text-gray-500"> {dayjs(selectedEvent.start).format('DD/MM/YYYY HH:mm')}</dd>
-    </div>
-
-    <div class="flex flex-col-reverse">
-      <dt class="text-sm font-medium text-gray-600">Fecha de fin</dt>
-      <dd class="text-xs text-gray-500">{dayjs(selectedEvent.end).format('DD/MM/YYYY HH:mm')}</dd>
-    </div>
-  </dl>
-</a>
+              <div className="w-full">
+                <div className="w-full p-6 bg-white rounded-lg shadow-lg">
+                  <h3 className="text-3xl font-semibold text-center text-gray-500 mt-0 mb-2">Evento</h3>
+                  <form className="px-0 pb-2">
+                    <div className="sm:flex sm:justify-between sm:gap-4">
+                      <div>
+                        <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
+                          {selectedEvent.title}
+                        </h3>
+                      </div>
+                    </div>
+                    <div className="mt-4">
+                      <p className="text-pretty text-sm text-gray-500">
+                        {selectedEvent.description}
+                      </p>
+                    </div>
+                    <dl className="mt-6 flex gap-4 sm:gap-6">
+                      <div className="flex flex-col-reverse">
+                        <dt className="text-sm font-medium text-gray-600">Fecha de inicio</dt>
+                        <dd className="text-xs text-gray-500"> {dayjs(selectedEvent.start).format('DD/MM/YYYY HH:mm')}</dd>
+                      </div>
+                      <div className="flex flex-col-reverse">
+                        <dt className="text-sm font-medium text-gray-600">Fecha de fin</dt>
+                        <dd className="text-xs text-gray-500">{dayjs(selectedEvent.end).format('DD/MM/YYYY HH:mm')}</dd>
+                      </div>
+                    </dl>
+                  </form>
+                </div>
+              </div>
           </div>
         )}
       </div>
